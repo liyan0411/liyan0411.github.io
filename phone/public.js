a406be709d01f1d4453a21d0a5b2913aa4ff69d7
liyan0411 <541267924@qq.com>
2017-05-11 15:16:15 +0800

    判断是否为微信浏览器

---
+++ phone/public.js
@@ -0,0 +1,14 @@
+// 设置不同屏幕下的rem
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            var clientHeight = docEl.clientHeight;
+            if (!clientWidth) return;
+            docEl.style.fontSize =20 * (clientWidth / 320) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
